85dd6970d0ad24e3a5da0d6219ca7768b9e9fda8
Lukepedr0 <pedro.bustamante@bagy.com.br>
2022-12-06 09:09:36 -0300

    remove some files

--- .next/static/webpack/webpack.c149a843461379ac.hot-update.js
+++ .next/static/webpack/webpack.c149a843461379ac.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3bf9710395fd77d6"; }
+/******/ 	__webpack_require__.h = function() { return "0a3f70f9ae86f9bb"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
